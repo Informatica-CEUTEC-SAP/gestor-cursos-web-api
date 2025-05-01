9f1931d4dc09950d4532529e7157743f089689e0
Ivan Cruz <ivancruzhn@gmail.com>
2025-04-30 19:52:24 -0600

    feat: creacion de solucion y proyecto web api

---
+++ GestorCursos.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace GestorCursos.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ GestorCursos/Program.cs
@@ -0,0 +1,25 @@
+#region Step 1: Configuration Setup
+var builder = WebApplication.CreateBuilder(args);
+#endregion Step 1: Configuration Setup
+    
+#region Step2: Service Registration
+#region Step2.1: Add services to the DI container.
+    
+#endregion Step2.1: Add services to the DI container.
+    
+#region Step2.2: Add database context
+    
+#endregion Step2.2: Add database context
+#endregion Step2: Service Registration
+    
+#region Step3: Build the application
+var app = builder.Build();
+#endregion Step3: Build the application
+
+#region Step4: Middleware Pipeline Configuration
+
+#endregion Step4: Middleware Pipeline Configuration
+
+#region Step5: Start the Application
+app.Run();
+#endregion Step5: Start the Application
